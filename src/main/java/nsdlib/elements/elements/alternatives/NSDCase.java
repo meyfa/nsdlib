50761677e5dfa33a783172850a825568a532ad12
JangoBrick <mail@jangobrick.de>
2017-02-22 15:37:52 +0100

    Add case and decision element types (alternatives)

---
+++ src/main/java/nsdlib/elements/elements/alternatives/NSDCase.java
@@ -0,0 +1,36 @@
+package nsdlib.elements.elements.alternatives;
+
+import java.util.Collection;
+
+import nsdlib.elements.NSDContainer;
+import nsdlib.elements.NSDElement;
+
+
+/**
+ * Represents a case / select / switch element, i.e. multiple possible execution
+ * lists of which one is chosen depending on some input or question.
+ */
+public class NSDCase extends NSDContainer<NSDContainer<NSDElement>>
+{
+    /**
+     * @param label The element's label.
+     * 
+     * @throws IllegalArgumentException If label is null (use the empty string).
+     */
+    public NSDCase(String label)
+    {
+        super(label);
+    }
+
+    /**
+     * @param label The element's label.
+     * @param children The element's initial child elements.
+     * 
+     * @throws IllegalArgumentException If label is null (use the empty string).
+     */
+    public NSDCase(String label,
+            Collection<? extends NSDContainer<NSDElement>> children)
+    {
+        super(label, children);
+    }
+}
---
+++ src/main/java/nsdlib/elements/elements/alternatives/NSDDecision.java
@@ -0,0 +1,69 @@
+package nsdlib.elements.elements.alternatives;
+
+import java.util.Collection;
+
+import nsdlib.elements.NSDContainer;
+import nsdlib.elements.NSDElement;
+
+
+/**
+ * Represents a decision element, i.e. an if-then-else construct. The else
+ * branch is optional.
+ */
+public class NSDDecision extends NSDElement
+{
+    private final NSDContainer<NSDElement> then, otherwise;
+
+    /**
+     * @param label The element's label.
+     * 
+     * @throws IllegalArgumentException If label is null (use the empty string).
+     */
+    public NSDDecision(String label)
+    {
+        this(label, null, null);
+    }
+
+    /**
+     * @param label The element's label.
+     * @param then The child elements for the "then" branch.
+     * 
+     * @throws IllegalArgumentException If label is null (use the empty string).
+     */
+    public NSDDecision(String label, Collection<? extends NSDElement> then)
+    {
+        this(label, then, null);
+    }
+
+    /**
+     * @param label The element's label.
+     * @param then The child elements for the "then" branch.
+     * @param otherwise The child elements for the "else" branch.
+     * 
+     * @throws IllegalArgumentException If label is null (use the empty string).
+     */
+    public NSDDecision(String label, Collection<? extends NSDElement> then,
+            Collection<? extends NSDElement> otherwise)
+    {
+        super(label);
+
+        this.then = new NSDContainer<>("T", then);
+        this.otherwise = new NSDContainer<>("F", otherwise);
+    }
+
+    /**
+     * @return The child elements for the "then" branch of this decision.
+     */
+    public NSDContainer<NSDElement> getThen()
+    {
+        return then;
+    }
+
+    /**
+     * @return The child elements for the "else" branch of this decision.
+     */
+    public NSDContainer<NSDElement> getElse()
+    {
+        return otherwise;
+    }
+}
